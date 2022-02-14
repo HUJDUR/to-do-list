80a397d95b242a94d6f17a9be3e300c6f3c028a7
HUJDUR <nadir.hujdur2002@gmail.com>
2022-02-14 21:00:06 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,42 +2,63 @@ import React, { Component } from 'react';
 import Aux from './HOC/Auxiliary';
 
 class App extends Component {
-	state = {
-		no: 'jes',
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			iteams: [1, 2, 3],
+		};
+	}
 
 	render() {
 		return (
 			<Aux>
-				<div class="container--main">
-					<header class="container--main__header">
-						<h1 class="container--main__header__text">To Do List</h1>
+				<div className="container--main">
+					<header className="container--main__header">
+						<h1 className="container--main__header__text">To Do List</h1>
 					</header>
-					<main class="to-do-list">There are no to-do items for today</main>
+					{this.state.iteams.length ? (
+						this.state.iteams.map((el) => {
+							return (
+								<div className="to-do-list__element">
+									<h2 className="to-do-list__element__text">Placeholder</h2>
+									<div
+										className="to-do-list__element__checkbox"
+										data-listnumber="0"
+									>
+										<div Name="to-do-list__element__checkbox__under"></div>
+									</div>
+								</div>
+							);
+						})
+					) : (
+						<main className="to-do-list">
+							There are no to-do items for today
+						</main>
+					)}
 				</div>
 
-				<footer class="container--options">
-					<div class="options--main">
-						<button class="btn btn--new-item popup">
+				<footer className="container--options">
+					<div className="options--main">
+						<button className="btn btn--new-item popup">
 							New to-do item
 							<img src="img/icons/plus1.svg" alt="Plus icon" class="icon" />
 						</button>
 					</div>
-					<div class="options--secondary">
-						<button class="btn btn--new-item">
+					<div className="options--secondary">
+						<button className="btn btn--new-item">
 							Options
 							<img
 								src="img/icons/options.svg"
 								alt="Options icon"
-								class="icon"
+								className="icon"
 							/>
 						</button>
-						<button class="btn btn--new-item">
+						<button className="btn btn--new-item">
 							Change theme
 							<img
 								src="img/icons/moon.svg"
 								alt="Change theme icon"
-								class="icon"
+								className="icon"
 							/>
 						</button>
 					</div>
