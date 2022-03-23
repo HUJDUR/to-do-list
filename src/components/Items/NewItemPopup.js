52c16a2067b35dddb4833bf847f7989bd343ce5d
HUJDUR <nadir.hujdur2002@gmail.com>
2022-03-23 21:57:35 +0100

    Update

--- src/components/Items/NewItemPopup.js
+++ src/components/Items/NewItemPopup.js
@@ -1,40 +1,51 @@
-import { useSpring, animated } from 'react-spring';
+import { useTransition, animated } from 'react-spring';
+import Aux from '../hoc/Auxiliary';
 
 const NewIteam = (props) => {
-	const animatedStyle = useSpring({
-		from: { opacity: !props.popupState ? 1 : 0 },
-		to: { opacity: props.popupState ? 1 : 0 },
+	const transition = useTransition(props.popupState, {
+		from: { opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
 	});
 	return (
-		<animated.div className="new-to-do-popup" style={animatedStyle}>
-			<button className="btn new-to-do-popup__exit popup">
-				<img
-					src="/img/icons/x.svg"
-					alt="Exit icon"
-					className="icon--exit"
-					onClick={props.exitHandler}
-				/>
-			</button>
+		<Aux>
+			{transition((style, item) => {
+				item ? (
+					<animated.div className="new-to-do-popup" style={style}>
+						<button className="btn new-to-do-popup__exit popup">
+							<img
+								src="/img/icons/x.svg"
+								alt="Exit icon"
+								className="icon--exit"
+								onClick={props.exitHandler}
+							/>
+						</button>
 
-			<form className="new-to-do-popup__form">
-				<label htmlFor="new-list-element" className="new-to-do-popup__label">
-					New To Do List Element
-				</label>
-				<input
-					type="text"
-					className="new-to-do-popup__input"
-					name="new-list-element"
-				/>
-			</form>
-			<button className="btn btn--add-element">
-				Add element
-				<img
-					src="img/icons/plus2.svg"
-					alt="Plus icon"
-					className="icon icon--add-element"
-				/>
-			</button>
-		</animated.div>
+						<form className="new-to-do-popup__form">
+							<label
+								htmlFor="new-list-element"
+								className="new-to-do-popup__label"
+							>
+								New To Do List Element
+							</label>
+							<input
+								type="text"
+								className="new-to-do-popup__input"
+								name="new-list-element"
+							/>
+						</form>
+						<button className="btn btn--add-element">
+							Add element
+							<img
+								src="img/icons/plus2.svg"
+								alt="Plus icon"
+								className="icon icon--add-element"
+							/>
+						</button>
+					</animated.div>
+				) : null;
+			})}
+		</Aux>
 	);
 };
 
